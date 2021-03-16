809f3d0917a2d86c425d800fe2ceb548612c9e23
Rodrigo Bussolo <rbussolo91@gmail.com>
2021-03-15 20:46:40 -0400

    Criado dois components, a barra lateral e o conteudo dos filmes, porém foi mantido no App o controle de estado da aplicação.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number;
+  handleClickButton: (number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
